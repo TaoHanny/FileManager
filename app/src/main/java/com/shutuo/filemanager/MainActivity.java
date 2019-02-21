d9c97ae189c5a1692472c277fcdb4266709d0525
honny <274140889@qq.com>
2019-02-21 18:18:02 +0800

    初始项目，可获取到U盘字符串

---
+++ app/src/main/java/com/shutuo/filemanager/MainActivity.java
@@ -0,0 +1,38 @@
+package com.shutuo.filemanager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        Button tv =  findViewById(R.id.sample_text);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,com.shutuo.filemanagement.MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
